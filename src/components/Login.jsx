b961dbe8da90c8458c441860ba267179c48663a8
Tushar Dubey <145278723+TusharDubey07@users.noreply.github.com>
2024-08-30 17:22:02 +0530

    added login api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3000/login', {
+    const response = await fetch('https://fantasy-collection-backend.onrender.com/api/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
